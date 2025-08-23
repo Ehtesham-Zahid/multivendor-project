6765595c24f2881285f7b7d171b0608f683db6b6
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-08-23 22:09:51 +0500

    Completed

--- backend/socket.js
+++ backend/socket.js
@@ -3,7 +3,7 @@ let io;
 const initSocket = (server) => {
   const { Server } = require("socket.io");
   io = new Server(server, {
-    cors: { origin: "http://localhost:5173", credentials: true },
+    cors: { origin: process.env.FRONTEND_URL, credentials: true },
   });
   return io;
 };
