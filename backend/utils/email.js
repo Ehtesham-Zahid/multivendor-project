b8d6cc94c71ae97cf0203f58c20f73845c9a9284
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-30 12:11:22 +0500

    Email Fixed

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,53 +1,34 @@
-// const { Resend } = require("resend");
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+import ErrorHandler from "./ErrorHandler.js";
 
-// const POST = async (details) => {
-//   const { to, subject, html } = details;
-
-//   try {
-//     const data = await resend.emails.send({
-//       from: "SwiftCart <swiftcart@resend.dev>", // or verified domain
-//       to,
-//       subject,
-//       html,
-//     });
-
-//     return new Response(JSON.stringify({ success: true, data }), {
-//       status: 200,
-//     });
-//   } catch (error) {
-//     return new Response(JSON.stringify({ success: false, error }), {
-//       status: 500,
-//     });
-//   }
-// };
-
-// module.exports = POST;
-
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
-
-async function sendMail(to, subject, html) {
+export const sendMail = async (options) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"SwiftCart" <ehteshamzahid313@gmail.com>`,
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || "465"),
+      secure: true,
+      service: process.env.SMTP_SERVICE,
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    const { to, subject, html } = options;
+
+    const mailOptions = {
+      from: `"SkillStack" <${process.env.SMTP_MAIL}>`,
       to,
       subject,
       html,
-    });
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
-}
+    };
 
-module.exports = sendMail;
+    await transporter.sendMail(mailOptions);
+
+    console.log("✅ Email sent successfully!");
+  } catch (error) {
+    throw new ErrorHandler(error.message, 500);
+  }
+};
